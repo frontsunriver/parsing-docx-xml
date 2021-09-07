afcda95fa8f386d896e2628378a6acfcd311dd56
sunriver <neymarjohn215@gmail.com>
2021-09-07 08:37:05 +0300

    change Form to console

--- src/main/java/com/mycompany/docxunzipper/Main.java
+++ src/main/java/com/mycompany/docxunzipper/Main.java
@@ -1,8 +1,15 @@
 package com.mycompany.docxunzipper;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.util.logging.Logger;
 
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
 import com.mycompany.xwpf.Form;
+import com.mycompany.xwpf.Helper;
 
 public class Main {
 
@@ -21,16 +28,22 @@ public class Main {
 //        System.out.println(Helper.getTextFromDocument(djmDocument));
     	
 //    	String userDirectory = System.getProperty("user.dir");
-//        File f = new File(userDirectory, "/src/main/resources/example.docx");
-//        
-//        FileInputStream fis = new FileInputStream(f); 
-//
-//        XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(fis));
-//	    Helper helper = new Helper();
-//	    
-//	    helper.makeXml(xdoc);
-    	Form f = new Form();
-    	f.show();
+    	try {
+    		File f = new File("doysa.docx");
+            FileInputStream fis = new FileInputStream(f); 
+            XWPFDocument xdoc = new XWPFDocument(OPCPackage.open(fis));
+    	    Helper helper = new Helper();
+    	    String xmlStr = helper.makeXml(xdoc);
+    	    
+    	    FileWriter myWriter = new FileWriter("content.xml");
+            myWriter.write(xmlStr);
+            myWriter.close();
+    	} catch(Exception e) {
+    		
+    	}
+        
+//    	Form f = new Form();
+//    	f.show();
 //        LOGGER.log(Level.INFO, "The text is: " + );
 //        LOGGER.log(Level.INFO, "All bold words in the document: " + Helper.getBoldWords(djmDocument).toString());
     }
